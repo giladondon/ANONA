34b7c65e7e94440852c89490a30b9456c9bd18bb
Gilad Barak <giladondon@gmail.com>
2017-03-23 19:57:57 +0200

    Work in class 23.3 - Sample taking almost completed

--- Sampling/Mouse.py
+++ Sampling/Mouse.py
@@ -30,72 +30,102 @@ from math import sqrt, pow
 # -------------- FUNCTIONS -------------------------
 
 
-def count_clicks_over_time(time_range):
-    """
-    :param time_range: time_range in seconds
-    :type time_range: int
-    :return right_clicks, left_clicks: count of clicks
-    :type return: tuple of int
-    """
-    right_clicks = 0
-    left_clicks = 0
-    state_right = GetKeyState(RIGHT_MOUSE_ID)
-    state_left = GetKeyState(LEFT_MOUSE_ID)
-    timer_end = time() + time_range
-    while time() < timer_end:
-        if GetKeyState(LEFT_MOUSE_ID) != state_left:
-            if state_left < 0:
-                left_clicks += 1
-            state_left = GetKeyState(LEFT_MOUSE_ID)
-        elif GetKeyState(RIGHT_MOUSE_ID) != state_right:
-            if state_right < 0:
-                right_clicks += 1
-            state_right = GetKeyState(RIGHT_MOUSE_ID)
+class CountClicksOverTime:
 
-    return right_clicks, left_clicks
+    def __init__(self):
+        self.result = 0
 
+    def execute(self, time_range):
+        """
+        :param time_range: time_range in seconds
+        :type time_range: int
+        :return right_clicks, left_clicks: count of clicks
+        :type return: tuple of int
+        """
+        right_clicks = 0
+        left_clicks = 0
+        state_right = GetKeyState(RIGHT_MOUSE_ID)
+        state_left = GetKeyState(LEFT_MOUSE_ID)
+        timer_end = time() + time_range
+        while time() < timer_end:
+            if GetKeyState(LEFT_MOUSE_ID) != state_left:
+                if state_left < 0:
+                    left_clicks += 1
+                state_left = GetKeyState(LEFT_MOUSE_ID)
+            elif GetKeyState(RIGHT_MOUSE_ID) != state_right:
+                if state_right < 0:
+                    right_clicks += 1
+                state_right = GetKeyState(RIGHT_MOUSE_ID)
 
-def mouse_distance_over_time(time_range):
-    """
-    :param time_range: time range in seconds
-    :type time_range: int
-    :return distance: distance mouse covered over time range
-    :type return: double
-    """
-    timer_end = time() + time_range
-    cursor = GetCursorPos()
-    distance = 0
+        self.result = right_clicks, left_clicks
+        return self.result
 
-    while time() < timer_end:
-        if GetCursorPos() != cursor:
-            x_delta = (cursor[0] - GetCursorPos()[0])
-            y_delta = (cursor[1] - GetCursorPos()[1])
-            distance += sqrt(pow(x_delta, 2) + pow(y_delta, 2))
-            cursor = GetCursorPos()
-
-    return distance
+    @staticmethod
+    def is_valid(result):
+        return True
 
 
-def mouse_speed_avg_over_time(time_range):
-    """
-    :param time_range: time range in seconds
-    :type time_range: int
-    :return speeds average: averages of mouse speeds over time
-    """
-    movements_count = 0
-    speeds_sum = 0
-    movement_timer = time()
+class MouseDistanceOverTime:
 
-    timer_ends = time() + time_range
-    cursor_pos = GetCursorPos()
-    while time() < timer_ends:
-        if cursor_pos != GetCursorPos():
-            movement_timer = time()
-        if cursor_pos == GetCursorPos():
-            speeds_sum += time() - movement_timer
-            movements_count += 1
+    def __init__(self):
+        self.result = 0
 
-    return speeds_sum / movements_count
+    def execute(self, time_range):
+        """
+        :param time_range: time range in seconds
+        :type time_range: int
+        :return distance: distance mouse covered over time range
+        :type return: double
+        """
+        timer_end = time() + time_range
+        cursor = GetCursorPos()
+        distance = 0
+
+        while time() < timer_end:
+            if GetCursorPos() != cursor:
+                x_delta = (cursor[0] - GetCursorPos()[0])
+                y_delta = (cursor[1] - GetCursorPos()[1])
+                distance += sqrt(pow(x_delta, 2) + pow(y_delta, 2))
+                cursor = GetCursorPos()
+
+        self.result = distance
+        return self.result
+
+    @staticmethod
+    def is_valid(result):
+        return result > 0
+
+
+class MouseSpeedAverageOverTime:
+
+    def __init__(self):
+        self.result = 0
+
+    def execute(self, time_range):
+        """
+        :param time_range: time range in seconds
+        :type time_range: int
+        :return speeds average: averages of mouse speeds over time
+        """
+        movements_count = 0
+        speeds_sum = 0
+        movement_timer = time()
+
+        timer_ends = time() + time_range
+        cursor_pos = GetCursorPos()
+        while time() < timer_ends:
+            if cursor_pos != GetCursorPos():
+                movement_timer = time()
+            if cursor_pos == GetCursorPos():
+                speeds_sum += time() - movement_timer
+                movements_count += 1
+
+        self.result = speeds_sum / movements_count
+        return self.result
+
+    @staticmethod
+    def is_valid(result):
+        return result > 0
 
 # --------------------------------------------------
 
--- Sampling/Sample.py
+++ Sampling/Sample.py
@@ -16,37 +16,16 @@ from multiprocessing.pool import ThreadPool
 # --------------------------------------------------
 
 # --------------- CONSTANTS ------------------------
-SAMPLES = {"Mouse Speed": Mouse.mouse_speed_avg_over_time,
-           "Mouse Clicks Count": Mouse.count_clicks_over_time,
-           "Mouse Distance Covered": Mouse.mouse_distance_over_time}
-# --------------------------------------------------
+SAMPLES = {"Mouse Speed": Mouse.MouseSpeedAverageOverTime(),
+           "Mouse Clicks Count": Mouse.CountClicksOverTime(),
+           "Mouse Distance Covered": Mouse.MouseDistanceOverTime()}
 
-# -------------- FUNCTIONS -------------------------
-
-
-def manage_threads(time_range, samples_func):
-    """
-    :param time_range: time range in seconds
-    :type time_range: int
-    """
-    pool = ThreadPool(processes=len(samples_func))
-
-    for key in samples_func.keys():
-        samples_func[key] = pool.apply_async(samples_func[key], (time_range, ))
-
-    for key in samples_func.keys():
-        samples_func[key] = samples_func[key].get()
-
-    return samples_func
-
-
-def generate_sample(sampling_dict):
-    return Sample(sampling_dict)
-
-
-def main():
-    print manage_threads(10, SAMPLES)
-
+TIME_RANGE_FOR_CLASS = 60*2
+TIME_RANGE_FOR_SAMPLE = 10
+MOUSE_SPEED = "Mouse Speed"
+MOUSE_CLICKS = "Mouse Clicks Count"
+MOUSE_DISTANCE = "Mouse Distance Covered"
+VALID = True
 # --------------------------------------------------
 
 # --------------- CLASS ----------------------------
@@ -60,6 +39,61 @@ class Sample:
         """
         self.sampling_dict = sampling_dict
         self.time_stamp = time()
+
+    def is_valid(self):
+        for aspect in self.sampling_dict.keys():
+            print self.sampling_dict[aspect]
+            if not SAMPLES[aspect].is_valid(self.sampling_dict[aspect]):
+                return SAMPLES[aspect].is_valid(self.sampling_dict[aspect])
+
+        return VALID
+
+# --------------------------------------------------
+
+
+# -------------- FUNCTIONS -------------------------
+
+
+def manage_threads(time_range, samples_func):
+    """
+    :param time_range: time range in seconds
+    :type time_range: int
+    """
+    pool = ThreadPool(processes=len(samples_func.keys()))
+    samples = {}
+
+    for aspect in samples_func.keys():
+        samples[aspect] = pool.apply_async(samples_func[aspect].execute, args=(time_range, ))
+
+    for key in samples_func.keys():
+        samples[key] = samples[key].get()
+
+    return samples
+
+
+def generate_sample(sampling_dict):
+    return Sample(sampling_dict)
+
+
+def make_sets_class(time_range):
+    """
+    :param time_range: time range in seconds
+    :type time_range: int
+    """
+    timer_ends = time() + time_range
+    samples = []
+
+    while time() < timer_ends:
+        current_sample = generate_sample(manage_threads(TIME_RANGE_FOR_SAMPLE, SAMPLES))
+        print current_sample.sampling_dict
+        if current_sample.is_valid():
+            samples.append(current_sample)
+
+    return samples
+
+
+def main():
+    print make_sets_class(TIME_RANGE_FOR_CLASS)
 
 # --------------------------------------------------
 
