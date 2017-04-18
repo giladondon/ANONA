519d6d0a2b4ec778e23590b937cee2a5fe0fcbd7
giladondon@gmail.com <takeIt123>
2017-04-18 20:43:05 +0300

    Some serious work done.
    Started working on client-server communication
    parsed text file back to sample
    And more.

--- Sampling/ServerCommun.py
+++ Sampling/ServerCommun.py
@@ -17,6 +17,9 @@ REGISTER_HEADER = "SIGN"
 VALID_SAMPLE_HEADER = "SMPT"
 NEW_SAMPLE_HEADER = "SMPN"
 LOG_OUT_HEADER = "EXIT"
+SEPARATOR = "$|"
+KB = 1024
+HALF_KB = 1024/2
 # --------------------------------------------------
 
 # -------------- IMPORTS & SETTINGS ----------------
@@ -36,18 +39,75 @@ def client_approach(conn):
     """
     while True:
         try:
-            loads(conn.recv(1024)).print_sample()
-            conn.send("OK")
+            raw_signal = conn.recv(HALF_KB)
+
         except error:
+            conn.close()
             return
+
+        finally:
+            anona_client_data = parse_anona_client_signal(raw_signal)
+            # anona_client[0] is the header from client signal, returned by parsing method
+            if anona_client_data[0] == REGISTER_HEADER:
+                pass
+
+            if anona_client_data[0] == VALID_SAMPLE_HEADER:
+                pass
+
+            if anona_client_data[0] == NEW_SAMPLE_HEADER:
+                pass
+
+            if anona_client_data[0] == NEW_SAMPLE_HEADER:
+                conn.close()
 
 
 def parse_anona_client_signal(data):
     """
     :param data: data from socket to client
     :type data: str
+    :return: tuple object with header and parsed data
     """
+    if data.startswith(REGISTER_HEADER):
+        return REGISTER_HEADER, parse_register_signal(data)
 
+    elif data.startswith(VALID_SAMPLE_HEADER):
+        return VALID_SAMPLE_HEADER, parse_sample_signal(data)
+
+    elif data.startswith(NEW_SAMPLE_HEADER):
+        return NEW_SAMPLE_HEADER, parse_sample_signal(data)
+
+    else:
+        return LOG_OUT_HEADER, parse_exit_signal(data)
+
+
+def parse_register_signal(data):
+    """
+    :param data: data from socket to client, starts with 'SIGN'
+    :type data: srt
+    :return: array of user data
+    """
+    data = data.split(SEPARATOR)[1:]  # Remove header cell
+    return data
+
+
+def parse_sample_signal(data):
+    """
+    :param data: data from socket to client, starts with 'SMPT'
+    :type data: str
+    :return: Sample object received from data
+    """
+    data = data.split(SEPARATOR)[1:]  # Remove header cell
+    data[0] = loads(data[0])  # data[0] contains pickled sample
+    return data[0]
+
+
+def parse_exit_signal(data):
+    """
+    :param data: data from socket to client, starts with 'SMPT'
+    :type data: str
+    :return: time of log out in str
+    """
+    return data.split(SEPARATOR)[1]  # data[1] contains time of log out
 # --------------------------------------------------
 
 # --------------- CLASS ----------------------------
