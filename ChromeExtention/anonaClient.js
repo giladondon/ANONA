80da0c78209f75d4a32f1f4aaf855d43e5a76177
Gilad <giladondon@gmail.com>
2017-04-27 16:26:12 +0300

    After taking a quick Google Chrome extension tutorial, I started coding,
    Manifest is on there with all icons for future and current need. adds a simple HTML popup for extension and started working on core code.

--- ChromeExtention/anonaClient.js
+++ ChromeExtention/anonaClient.js
@@ -2,4 +2,21 @@
 // Using a sophisticated AI mechanism Anona can learn one's patterns of behavior
 // and Alert when a stranger is using the chat application
 
-chrome.input.ime.addListener()
+alert("Anona AI can now know when you are chatting on whatsapp web")
+
+var context_id = -1;
+
+chrome.input.ime.onFocus.addListener(function(context) {
+    context_id = context.contextID;
+});
+
+chrome.input.ime.onKeyEvent.addListener(
+    function(engineID, keyData) {
+        if (keyData.type == "keydown" && keyData.key.match(/^[a-z]$/)) {
+            chrome.input.ime.commitText({"contextID": context_id,
+                                         "text": keyData.key.toUpperCase()});
+            return true;
+        } else {
+            return false;
+        }
+});
